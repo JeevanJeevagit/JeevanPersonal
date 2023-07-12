fc4a5a370c06bba3384a30d3e4e828da21077980
Jeevan <Jeevankumar15166ec@gmail.com>
2023-07-12 21:57:52 +0530

    sample 2nd commit

---
+++ src/Demo1/sampleEx2.java
@@ -0,0 +1,10 @@
+package Demo1;
+
+public class sampleEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is sample example 2nd");
+	}
+
+}
