9d0766209e9be672cb492e6f07950717581c791a
Jeevan <Jeevankumar15166ec@gmail.com>
2023-07-13 10:53:21 +0530

    Sample 3rd example

---
+++ src/Demo1/SampleEx3.java
@@ -0,0 +1,10 @@
+package Demo1;
+
+public class SampleEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the 3rd samle example");
+	}
+
+}
